1b6a2d3c01f06ed6c7d6278e64a9367f816076e7
VindulaNR <e16319@eng.pdn.ac.lk>
2021-03-02 02:16:24 +0530

    March1 2nd

--- codes/mobile app/lib/Battling/host_battle_3teams_normal.dart
+++ codes/mobile app/lib/Battling/host_battle_3teams_normal.dart
@@ -1,24 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:xtag_demo/Battling/player_parameter.dart';
-import 'package:xtag_demo/Battling/players_data_2teams_resc.dart';
 import 'package:xtag_demo/Battling/players_data_normal3.dart';
-import 'package:xtag_demo/Battling/time_display.dart';
 import 'package:xtag_demo/Model/player1.dart';
 import 'package:xtag_demo/Model/match.dart';
-import 'package:xtag_demo/PlayModes/timern.dart';
 import 'package:xtag_demo/PlayModes/timer2.dart';
 import 'package:xtag_demo/PlayModes/timerh.dart';
-
-import 'package:xtag_demo/Results/result_team3_normal.dart';
 import 'package:xtag_demo/Services/database.dart';
-import 'package:xtag_demo/TeamSocres/team1.dart';
-import 'package:xtag_demo/TeamSocres/team2.dart';
-import 'package:xtag_demo/TeamSocres/team3.dart';
-
 import 'battle_started_mas.dart';
 
 class Host3teamNormalUntilStart extends StatefulWidget {
