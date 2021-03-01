68860a1ef7f5fc75c466798b481ec0c7e7184a43
VindulaNR <e16319@eng.pdn.ac.lk>
2021-03-02 02:27:10 +0530

    March1 2nd

--- codes/mobile app/lib/Battling/host_battle_3teams_surv.dart
+++ codes/mobile app/lib/Battling/host_battle_3teams_surv.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:xtag_demo/Battle/creat_game.dart';
 import 'package:xtag_demo/Battling/player_parameter.dart';
 import 'package:xtag_demo/Battling/players_data_3teams_surv.dart';
-import 'package:xtag_demo/Battling/players_data_free4all_name.dart';
 import 'package:xtag_demo/Battling/time_display.dart';
-import 'package:xtag_demo/Results/result_3teams_surv.dart';
-
-import 'battle_started_mas.dart';
+import 'package:xtag_demo/Model/player1.dart';
+import 'package:xtag_demo/Model/match.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:xtag_demo/Services/database.dart';
 
 class Host3teamSurvlUntilStart extends StatefulWidget {
   @override
@@ -14,8 +13,15 @@ class Host3teamSurvlUntilStart extends StatefulWidget {
       _Host3teamSurvlUntilStartState();
 }
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
 class _Host3teamSurvlUntilStartState extends State<Host3teamSurvlUntilStart> {
   //print(Match.mid);
+  final bool isBattlefinished = true;
+  int _shootid = 0;
+  int damage;
+  int teamid;
+  int tempid;
   @override
   Widget build(BuildContext context) {
     //print(Match.mid);
@@ -70,30 +76,157 @@ class _Host3teamSurvlUntilStartState extends State<Host3teamSurvlUntilStart> {
               child: JoinedPlayers3teamsSurv(),
             ),
             Container(
-              margin: const EdgeInsets.only(top: 20.0, right: 90.0, left: 90.0),
+              margin: const EdgeInsets.only(top: 10.0, right: 90.0, left: 90.0),
               child: RaisedButton(
                 shape: RoundedRectangleBorder(
                     side: BorderSide(
                       color: Colors.deepPurple[900],
                     ),
                     borderRadius: BorderRadius.circular(20.0)),
+                onPressed: () async {
+                  //decrease the health**************************************************************************************
+                  User user = _auth.currentUser;
+                  String hisuid;
+                  Player1.health = Player1.health - damage;
+                  Player1.deaths = Player1.deaths + damage;
+                  print(
+                      'damage $damage , team $teamid , tempid $tempid  current health:${Player1.health} player team:${Player1.team}');
+
+                  try {
+                    await DatabaseServices(uid: user.uid)
+                        .upadtenestedplayersdata(
+                            Match.mid, 'health', Player1.health);
+                    print(Player1.health);
+                  } catch (e) {
+                    print(e.toString());
+                  }
+
+                  //increase the deaths
+                  try {
+                    await DatabaseServices(uid: user.uid)
+                        .upadtenestedplayersdata(
+                            Match.mid, 'deaths', Player1.deaths);
+                    print(Player1.deaths);
+                  } catch (e) {
+                    print(e.toString());
+                  }
+                  print(Match.mid);
+
+                  //get  his id
+                  try {
+                    hisuid = await DatabaseServices(uid: user.uid)
+                        .getshootedplayerdata(Match.mid, teamid, tempid);
+                    print(hisuid);
+                  } catch (e) {
+                    print(e.toString());
+                  }
+                  //set his data
+                  try {
+                    await DatabaseServices(uid: user.uid)
+                        .updatehisdata(Match.mid, hisuid, Player1.team);
+                  } catch (e) {
+                    print(e.toString());
+                  }
+
+                  //set my data
+                  try {
+                    await DatabaseServices(uid: user.uid)
+                        .setmyshotdata(Match.mid, hisuid);
+                  } catch (e) {
+                    print(e.toString());
+                  }
+                  //increase the enemy player score
+                  try {
+                    await DatabaseServices(uid: user.uid)
+                        .increasehisscorre(hisuid, Match.mid, damage);
+                  } catch (e) {
+                    print(e.toString());
+                  }
+
+                  //respan the player
+                  if (Player1.health <= 0) {
+                    await Future.delayed(Duration(seconds: 10));
+                    Player1.health = 5;
+                    try {
+                      await DatabaseServices(uid: user.uid)
+                          .upadtenestedplayersdata(
+                              Match.mid, 'health', Player1.health);
+                      print(Player1.health);
+                    } catch (e) {
+                      print(e.toString());
+                    }
+                  }
+                },
                 child: Row(children: <Widget>[
                   //width: 80.0,
                   Container(
-                    child: Text(' Go & See the Results '),
+                    child: Text('      Get hit '),
                   ),
                 ]),
-                color: Colors.red[900],
-                onPressed: () {
-                  print(isBattlefinished);
-                  if (!isBattlefinished) {
-                    print('battle Ended');
-                    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
-                      print('sdds');
-                      return Result3teamSurv();
-                    }));
-                  }
-                },
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              height: 20.0,
+              width: 30.0,
+              child: Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  //inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  //validator: numberValidator,,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 10.0,
+                  ),
+                  onChanged: (val) {
+                    damage = int.parse(val);
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 2.0,
+            ),
+            Container(
+              color: Colors.white,
+              height: 20.0,
+              width: 30.0,
+              child: Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  //inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  //validator: numberValidator,,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12.0,
+                  ),
+                  onChanged: (val) {
+                    teamid = int.parse(val);
+                    ;
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 2.0,
+            ),
+            Container(
+              color: Colors.white,
+              height: 20.0,
+              width: 30.0,
+              child: Container(
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  //inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                  //validator: numberValidator,,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12.0,
+                  ),
+                  onChanged: (val) {
+                    tempid = int.parse(val);
+                  },
+                ),
               ),
             ),
           ],
